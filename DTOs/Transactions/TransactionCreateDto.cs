342fb1450acc24e45721360efe4f95c2c10c88b5
Pague, Lloyd <kristianpague@gmail.com>
2026-07-08 23:53:23 +0800

    refactor: update TransactionCreateDto and TransactionUpdateDto to use TransactionType enum

--- DTOs/Transactions/TransactionCreateDto.cs
+++ DTOs/Transactions/TransactionCreateDto.cs
@@ -1,11 +1,12 @@
-﻿namespace JelycoWarehouse.DTOs.Transactions
+﻿using JelycoWarehouse.Enums;
+
+namespace JelycoWarehouse.DTOs.Transactions
 {
     public class TransactionCreateDto
     {
         public int ItemId { get; set; }
         public int LocationId { get; set; }
         public int Quantity { get; set; }
-        public string? Type { get; set; } // "IN" or "OUT"
-        public DateTime Date { get; set; }
+        public TransactionType Type { get; set; }
     }
 }
--- DTOs/Transactions/TransactionUpdateDto.cs
+++ DTOs/Transactions/TransactionUpdateDto.cs
@@ -1,11 +1,13 @@
-﻿namespace JelycoWarehouse.DTOs.Transactions
+﻿using JelycoWarehouse.Enums;
+
+namespace JelycoWarehouse.DTOs.Transactions
 {
     public class TransactionUpdateDto
     {
         public int ItemId { get; set; }
         public int LocationId { get; set; }
         public int Quantity { get; set; }
-        public string Type { get; set; } = string.Empty;
+        public TransactionType Type { get; set; }
         public DateTime Date { get; set; }
     }
 }
