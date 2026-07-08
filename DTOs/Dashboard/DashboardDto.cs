8b541fc18de3aac2ec2934ddc6401e226338a6a5
Pague, Lloyd <kristianpague@gmail.com>
2026-07-08 23:53:14 +0800

    feat: add DashboardDto for dashboard data representation

---
+++ DTOs/Dashboard/DashboardDto.cs
@@ -0,0 +1,9 @@
+﻿namespace JelycoWarehouse.DTOs.Dashboard;
+
+public class DashboardDto
+{
+    public int TotalItems { get; set; }
+    public int TotalStock { get; set; }
+    public int TotalIn { get; set; }
+    public int TotalOut { get; set; }
+}
