59085ab358fef5e99f61026ceb5ed7bfb7e25099
Pague, Lloyd <kristianpague@gmail.com>
2026-07-12 23:44:43 +0800

    Update assembly information and add new migrations
    - Updated AssemblyInformationalVersion to "1.0.0+9ef9d67b730d1ed70e928af9647430cd3e053451".
    - Modified sourcelink.json to reflect the new commit hash.
    - Added new migrations: AddItemColor, RenameUnitPriceToCostPriceAndAddColor, and AddSupplierDeliveryCosts.
    - Updated XML documentation to include new migration methods.
    - Updated various cache files and binary outputs to reflect the changes.

--- obj/Debug/net8.0/JelycoWarehouse.AssemblyInfo.cs
+++ obj/Debug/net8.0/JelycoWarehouse.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JelycoWarehouse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8020fb55dfd2467ea539336293a20dc682bce7d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ef9d67b730d1ed70e928af9647430cd3e053451")]
 [assembly: System.Reflection.AssemblyProductAttribute("JelycoWarehouse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JelycoWarehouse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
