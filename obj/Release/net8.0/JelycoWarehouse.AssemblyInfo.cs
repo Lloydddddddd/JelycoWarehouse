cab1b65cdd14dec9f1a5447513ba574fc3f94993
Pague, Lloyd <kristianpague@gmail.com>
2026-07-14 18:05:06 +0800

    Update assembly information and migration documentation
    - Updated AssemblyInformationalVersion in JelycoWarehouse.AssemblyInfo.cs to "1.0.0+cbe24f70a7b458e8c694290c3b28b89b39179030".
    - Modified JelycoWarehouse.AssemblyInfoInputs.cache and JelycoWarehouse.csproj.CoreCompileInputs.cache with new hash values.
    - Updated JelycoWarehouse.sourcelink.json to reflect the new commit hash.
    - Added new migration methods and documentation for AddTransactionTypeEnum, AddItemColor, RenameUnitPriceToCostPriceAndAddColor, AddSupplierDeliveryCosts, LinkTransactionsToSupplierDeliveries, AddWarehouseReleases, and LinkTransactionsToWarehouseReleases in JelycoWarehouse.xml.
    - Updated binary files: JelycoWarehouse.dll, JelycoWarehouse.pdb, apphost.exe, and cache files for both debug and release configurations.

--- obj/Release/net8.0/JelycoWarehouse.AssemblyInfo.cs
+++ obj/Release/net8.0/JelycoWarehouse.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JelycoWarehouse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8020fb55dfd2467ea539336293a20dc682bce7d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbe24f70a7b458e8c694290c3b28b89b39179030")]
 [assembly: System.Reflection.AssemblyProductAttribute("JelycoWarehouse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JelycoWarehouse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
