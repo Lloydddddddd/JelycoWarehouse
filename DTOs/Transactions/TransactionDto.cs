a29acbcfebe21c9e8c59b54cb2744cfbd007bcfb
Pague, Lloyd <kristianpague@gmail.com>
2026-07-22 14:28:49 +0800

    feat: remove LocationId and LocationName from TransactionDto

--- DTOs/Transactions/TransactionDto.cs
+++ DTOs/Transactions/TransactionDto.cs
@@ -3,12 +3,15 @@
     public class TransactionDto
     {
         public int Id { get; set; }
+
         public int ItemId { get; set; }
+
         public string ItemName { get; set; } = string.Empty;
-        public int LocationId { get; set; }
-        public string LocationName { get; set; } = string.Empty;
+
         public int Quantity { get; set; }
+
         public string Type { get; set; } = string.Empty;
+
         public DateTime Date { get; set; }
     }
 }
